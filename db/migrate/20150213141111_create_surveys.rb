99436479d275b36d9131b2bd4c048dc74280374a
Elliott Young & Kara A Carrell <pair+elliottayoung.karaajc@devbootcamp.com>
2015-02-13 15:01:03 -0600

    adding views for users, and survey

--- db/migrate/20150213141111_create_surveys.rb
+++ db/migrate/20150213141111_create_surveys.rb
@@ -2,6 +2,7 @@ class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
       t.string     :ref_code
+      t.string     :survey_name
       t.belongs_to :user
 
       t.timestamps
