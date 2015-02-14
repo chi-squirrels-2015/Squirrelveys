1ada24dbfcb06b1453a99c0198b3c9a7f186745e
Kara A Carrell <karaAJC@gmail.com>
2015-02-14 15:02:17 -0600

    pseudocoding AJAX, editing surveyshow to render non-editable survey to logged out user

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+//CREATING A NEW SURVEY
+
+
+//EDITING AN EXISTING SURVEY
+// create a listening event for editing a survey for  the user who is logged in
+// when clicking the edit question button, the text will become editable inline
+
+//on the button, remove the question text, then render an input text box with question in it
+
+// when clicking the edit answer button, the answer will become editable.
+
+// when the creator wants to add a new answer or question,
+
 });
